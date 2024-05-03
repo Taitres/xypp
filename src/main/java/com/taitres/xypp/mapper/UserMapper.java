3a560b38bf192ef682b01452001fb36e203b61d5
Dailin Liu <920643082@qq.com>
2024-05-03 19:56:32 +0800

    dockerfile

---
+++ src/main/java/com/taitres/xypp/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.taitres.xypp.mapper;
+
+import com.taitres.xypp.model.domain.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author Marlin
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2024-05-02 21:52:48
+* @Entity com.taitres.xypp.model.domain.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
